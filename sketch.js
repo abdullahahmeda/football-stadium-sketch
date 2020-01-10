e6c6cdbb04664e1f42db30e3541a406a8f044fc4
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2020-01-10 10:17:01 +0200

    Added comments & deleted unnecessary code & used relative dimens

--- sketch.js
+++ sketch.js
@@ -1,101 +1,91 @@
 const canvasSketch = require("canvas-sketch");
-const { lerp } = require("canvas-sketch-util/math");
-const random = require("canvas-sketch-util/random");
-const palettes = require("nice-color-palettes");
+
+// For good results make width from (height * 2.1) to (height * 2.3)
 
 const settings = {
-  dimensions: [2048, 900],
+  dimensions: [720, 300],
   orientation: "landscape",
   units: "px",
   pixelsPerInch: 300
 };
 
-random.setSeed(random.getRandomSeed());
-console.log(random.getSeed());
-
 const sketch = () => {
-  const colorCount = random.rangeFloor(1, 6);
-  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
-  const margin = 400;
-
   return ({ context, width, height }) => {
-    const createGrid = () => {
-      const points = [];
-      const count = 6;
-
-      for (let r = 0; r < count; r++) {
-        for (let c = 0; c < count; c++) {
-          const u = count <= 1 ? 0.5 : r / (count - 1);
-          const v = count <= 1 ? 0.5 : c / (count - 1);
-          const x = lerp(margin, width - margin, u);
-          const y = lerp(margin, height - margin, v);
-          points.push([x, y]);
-        }
-      }
-
-      return points;
-    };
-
     context.fillStyle = "green";
-    context.fillRect(0, 0, 2048, 900);
+    // Pitch
+    context.fillRect(0, 0, width, height);
     context.fill();
 
-    context.lineWidth = 10;
+    context.lineWidth = width * 0.004;
     context.strokeStyle = "white";
     context.stroke();
 
-    context.moveTo(0, 900 / 5);
-    context.lineTo(250, 900 / 5);
-    context.lineTo(250, (4 * 900) / 5);
-    context.lineTo(0, (4 * 900) / 5);
+    // Outer Penalty line (left)
+    context.moveTo(0, height / 5);
+    context.lineTo(width * 0.122, height / 5);
+    context.lineTo(width * 0.122, (4 * height) / 5);
+    context.lineTo(0, (4 * height) / 5);
 
-    context.moveTo(0, (1.5 * 900) / 5);
-    context.lineTo(150, (1.5 * 900) / 5);
-    context.lineTo(150, ((4 - 0.5) * 900) / 5);
-    context.lineTo(0, ((4 - 0.5) * 900) / 5);
+    // Inner Penalty line (left)
+    context.moveTo(0, (1.5 * height) / 5);
+    context.lineTo(width * 0.073242, (1.5 * height) / 5);
+    context.lineTo(width * 0.073242, ((4 - 0.5) * height) / 5);
+    context.lineTo(0, ((4 - 0.5) * height) / 5);
 
-    context.moveTo(2048, 900 / 5);
-    context.lineTo(2048 - 250, 900 / 5);
-    context.lineTo(2048 - 250, (4 * 900) / 5);
-    context.lineTo(2048, (4 * 900) / 5);
+    // Outer Penalty line (right)
+    context.moveTo(width, height / 5);
+    context.lineTo(width - width * 0.122, height / 5);
+    context.lineTo(width - width * 0.122, (4 * height) / 5);
+    context.lineTo(width, (4 * height) / 5);
     context.stroke();
 
-    context.moveTo(2048, (1.5 * 900) / 5);
-    context.lineTo(2048 - 150, (1.5 * 900) / 5);
-    context.lineTo(2048 - 150, ((4 - 0.5) * 900) / 5);
-    context.lineTo(2048, ((4 - 0.5) * 900) / 5);
+    // Inner Penalty line (right)
+    context.moveTo(width, (1.5 * height) / 5);
+    context.lineTo(width - width * 0.073242, (1.5 * height) / 5);
+    context.lineTo(width - width * 0.073242, ((4 - 0.5) * height) / 5);
+    context.lineTo(width, ((4 - 0.5) * height) / 5);
     context.stroke();
 
+    // Big center circle
     context.beginPath();
-    context.arc(width / 2, height / 2, 900 / 5, 0, Math.PI * 2, true);
+    context.arc(width / 2, height / 2, height / 5, 0, Math.PI * 2, true);
     context.stroke();
 
+    // Small center circle
+    context.beginPath();
+    context.arc(width / 2, height / 2, width * 0.0048828, 0, Math.PI * 2, true);
+    context.stroke();
+
+    // Center line
     context.moveTo(width / 2, 0);
     context.lineTo(width / 2, height);
     context.stroke();
 
+    // Penalty half circle (left)
     context.beginPath();
     context.arc(
-      250,
+      width * 0.122,
       height / 2,
-      (0.5 * 900) / 5,
+      (0.5 * height) / 5,
       Math.PI / 2,
       Math.PI * 1.5,
       true
     );
     context.stroke();
 
+    // Penalty half circle (left)
     context.beginPath();
     context.arc(
-      width - 250,
+      width - width * 0.122,
       height / 2,
-      (0.5 * 900) / 5,
+      (0.5 * height) / 5,
       Math.PI / 2,
       Math.PI * 1.5,
       false
     );
     context.stroke();
 
+    // Out line
     context.moveTo(0, 0);
     context.lineTo(0, height);
     context.lineTo(width, height);
@@ -103,20 +93,20 @@ const sketch = () => {
     context.closePath();
     context.stroke();
 
-    context.font = "30px Helvetica";
+    context.font = `${width * 0.014648}px Helvetica`;
     context.fillStyle = "white";
-    context.fillText("By: Abdullah Ahmed", width - 350, height - 50);
-
-    context.font = "18px Helvetica";
     context.fillText(
-      "https://github.com/abdullahahmeda",
-      width - 350,
-      height - 30
+      "By: Abdullah Ahmed",
+      width - width * 0.17,
+      height - height * 0.0555
     );
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, 10, 0, Math.PI * 2, true);
-    context.stroke();
+    context.font = `${width * 0.008789}px Helvetica`;
+    context.fillText(
+      "https://github.com/abdullahahmeda",
+      width - width * 0.17,
+      height - height * 0.0333
+    );
   };
 };
 
